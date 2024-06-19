11a2d0a109c03bbbb7f01130f150eb6d91b2eb98
Mario Polchert <mario.polchert@gmail.com>
2024-06-19 17:31:23 +0200

    update-19.06.

--- library.sql
+++ library.sql
@@ -1,37 +1,18 @@
+DROP DATABASE IF EXISTS library;
+
+CREATE DATABASE IF NOT EXISTS library DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+
 -- Select the database
-USE Library;
+USE library;
 
--- members table
-CREATE TABLE IF NOT EXISTS members (
-    member_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    MemberCardNumber CHAR(14) NOT NULL UNIQUE,
-    name VARCHAR(100) NOT NULL,
-    surname VARCHAR(100) NOT NULL,
-    address VARCHAR(100),
-    phone VARCHAR(12),
-    email VARCHAR(50) NOT NULL UNIQUE
+-- Create genres table
+CREATE TABLE IF NOT EXISTS genres (
+    genre_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB;
 
--- borrows table
-CREATE TABLE IF NOT EXISTS borrows (
-    borrow_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    borrow_date DATE NOT NULL,
-    return_date DATE NOT NULL,
-    fine DECIMAL(10,2) NOT NULL,
-    BookCopy_id INT UNSIGNED,
-    MemberCardNumber CHAR(14),
-    FOREIGN KEY (BookCopy_id) REFERENCES bookCopy(BookCopy_id),
-    FOREIGN KEY (MemberCardNumber) REFERENCES members(MemberCardNumber)
-) ENGINE=InnoDB;
-
--- bookCopy table
-CREATE TABLE IF NOT EXISTS bookCopy (
-    BookCopy_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    book_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
-) ENGINE=InnoDB;
-
--- books table
+-- Create books table
 CREATE TABLE IF NOT EXISTS books (
     book_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -44,9 +25,32 @@ CREATE TABLE IF NOT EXISTS books (
     FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
 ) ENGINE=InnoDB;
 
+-- Create bookCopy table
+CREATE TABLE IF NOT EXISTS bookCopy (
+    BookCopy_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    book_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+) ENGINE=InnoDB;
 
--- genres table
-CREATE TABLE IF NOT EXISTS genres (
-    genre_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+-- Create members table
+CREATE TABLE IF NOT EXISTS members (
+    member_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    MemberCardNumber CHAR(14) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
+    address VARCHAR(100),
+    phone VARCHAR(12),
+    email VARCHAR(50) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
+-- Create borrows table
+CREATE TABLE IF NOT EXISTS borrows (
+    borrow_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    borrow_date DATE NOT NULL,
+    return_date DATE NOT NULL,
+    fine DECIMAL(10,2) NOT NULL,
+    BookCopy_id INT UNSIGNED,
+    MemberCardNumber CHAR(14),
+    FOREIGN KEY (BookCopy_id) REFERENCES bookCopy(BookCopy_id),
+    FOREIGN KEY (MemberCardNumber) REFERENCES members(MemberCardNumber)
 ) ENGINE=InnoDB;
---
+++ zadaca_videoteka.md
@@ -0,0 +1,12 @@
+### Baza podataka za poslovanje jedne videoteke.
+- Kreirajte ER dijagram za poslovanje jedne videoteke.
+- Videoteka članovima izdaje članske iskaznice te se na temelju članskog broja osoba identificira kako bi mogla posuditi filmove.
+- Filmovi su složeni po žanrovima.
+- Videoteka ima definiran cjenik za izdavanje hit filma, film koji nije hit te starog filma.
+- Jedan film može biti na DVD-u, BlueRay-u ili VHS-u.
+- Film se posđuje na rok od jednog dana I ako ga član ne vrati u navedeno vrijeme, zaračunava mu se zakasnina.
+
+### Dopuna zadace
+- Svaki film ima zalihu dostupnih kopija po mediju za koji je dostupan
+- Svaka fizicka kopija filma ima svoj jedinstveni identifikacijski broj (barcode) kako bi se mogla pratiti
+- Clan od jednom moze posuditi vise od jednog filma
